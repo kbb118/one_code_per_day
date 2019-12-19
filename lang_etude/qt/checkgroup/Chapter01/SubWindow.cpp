6a84bea001f1f9502dec79ec823c337a483609e7
kbb <kbbkbb524+oss@gmail.com>
2019-12-20 03:30:35 +0900

    scrollArea を vlayout->addWidget したら行けた!

--- lang_etude/qt/checkgroup/Chapter01/SubWindow.cpp
+++ lang_etude/qt/checkgroup/Chapter01/SubWindow.cpp
@@ -4,21 +4,51 @@
 #include <QDebug>
 
 SubWindow::SubWindow(const QString& name, QWidget *parent) :
-        QWidget(parent),
-        mEditButton(new QPushButton(this)),
-        mRemoveButton(new QPushButton(this)),
-        mCheckBox(new QCheckBox(this)),
-        mVLayout(new QVBoxLayout(this))
+        QWidget(parent)
 {
-    setName(name);
+    /*
+      this-superVlayout
+      \_w1
+        \_w1_1-vlayout
+      \_w2
+      \_w3
+     */
 
+
+    auto w1 = new QWidget(this);
+    auto w1_1 = new QWidget(w1);
+
+    auto scrl = new QScrollArea(w1);
+    scrl->setWidgetResizable(true);
+
+    // w の子にする。
+    mCheckBox = new QCheckBox(w1_1);
+    mEditButton = new QPushButton(w1_1);
+    mRemoveButton = new QPushButton(w1_1);
+    mVLayout = new QVBoxLayout(w1_1);
+    setName(name);
     mVLayout->addWidget(mCheckBox);
     mVLayout->addWidget(mEditButton);
     mVLayout->addWidget(mRemoveButton);
+    mVLayout->setSizeConstraint(QLayout::SetMinimumSize);
 
     for ( int i=0; i<30; i++ )
     {
-        mVLayout->addWidget(new QCheckBox(QString::number(i), this));
+        auto c = new QCheckBox(QString::number(i), w1_1);
+        mVLayout->addWidget(c);
+    }
+    scrl->setWidget(w1_1);
+
+    auto superLayout = new QVBoxLayout(this);
+
+    //ここで w1_1 を add するとおかしくなる。
+    superLayout->addWidget(scrl);
+
+    //w2, w3...
+    for ( int i=0; i<3; i++ )
+    {
+        auto c = new QCheckBox("super", this);
+        superLayout->addWidget(c);
     }
 
     connect(mEditButton, &QPushButton::clicked, this, &SubWindow::rename);
--- lang_etude/qt/checkgroup/Chapter01/ch01-todo.pro
+++ lang_etude/qt/checkgroup/Chapter01/ch01-todo.pro
@@ -5,7 +5,7 @@
 #-------------------------------------------------
 
 QT       += core gui
-CONFIG   += c++14
+CONFIG   += c++14 debug
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
