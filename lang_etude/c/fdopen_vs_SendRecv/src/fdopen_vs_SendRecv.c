281981072c38d75db7fab8f18a819cf1212145ca
kob <kob@wsl>
2021-07-10 23:53:32 +0900

    msgbuf の今の recv_line には無駄が多い

---
+++ lang_etude/c/fdopen_vs_SendRecv/src/fdopen_vs_SendRecv.c
@@ -0,0 +1,374 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+
+#include "logger.h"
+
+static inline char *
+split_str(char *str, char c)
+{
+    // c must exist in str!
+    char *ret = strchr(str, c);
+    *ret = '\0';
+    return ret + 1;
+}
+
+typedef struct {
+    char *buf;        // buffer must be passed from outside.
+    int buflen;
+
+    int  start;        // data is read/write but not processed part of buffer.
+    int  end;          // end of data
+
+    int  fd;
+} MsgBuf;
+
+typedef enum {
+    MSGBUF_ERR = 0,
+    MSGBUF_PEER_ALIVE,
+    MSGBUF_PEER_DEAD
+} MsgRet;
+
+void
+msg_init(MsgBuf *msg, char *buf, int buflen, int fd)
+{
+    msg->buf = buf;
+    msg->buflen = buflen;
+    msg->start = 0;
+    msg->end = 0;
+    msg->fd = fd;
+}
+
+static MsgRet
+_msg_send(MsgBuf *msg)
+{
+    if ( send(msg->fd, msg->buf, msg->end, MSG_NOSIGNAL) != msg->end )
+    {
+        perror("send");
+        return (errno == EPIPE) ? MSGBUF_PEER_DEAD : MSGBUF_ERR;
+    }
+
+    msg->start = 0;
+    msg->end = 0;
+    return MSGBUF_PEER_ALIVE;
+}
+
+Logger *gLogger;
+
+static MsgRet
+_msg_recv(MsgBuf *msg)
+{
+    int i;
+    for ( i = msg->start; i < msg->end; i++ )
+    {
+        if ( msg->buf[i] == '\n' )
+        {
+            return MSGBUF_PEER_ALIVE;
+        }
+    }
+    if ( i == msg->end )
+    {
+        int size = msg->end - msg->start;
+        // If bufsize is big enough, overlap doesn't occur...
+        // memmove(msg->buf, msg->buf + msg->start, size);
+        memcpy(msg->buf, msg->buf + msg->start, size);
+        msg->start = 0;
+        msg->end = size;
+
+        //logger_start(gLogger);
+        size = recv(msg->fd, msg->buf + msg->end, msg->buflen - size, 0);
+        //logger_end(gLogger);
+        if ( size < 0 )
+        {
+            perror("recv");
+            return MSGBUF_ERR;
+        }
+        else if ( size == 0 )
+        {
+            return MSGBUF_PEER_DEAD;
+        }
+
+        msg->end += size;
+    }
+    return MSGBUF_PEER_ALIVE;
+}
+
+MsgRet
+msg_send_line(MsgBuf *msg,
+              char   *line,
+              int    len)    /* [i] */
+{
+    MsgRet ret = MSGBUF_PEER_ALIVE;
+    if ( strcmp(line, "\n") == 0 )
+    {
+        msg->buf[msg->end++] = '\n';
+        return _msg_send(msg);
+    }
+
+    int capacity = msg->buflen - msg->end;
+
+    // Buffer is full. Send buffer and retry appending
+    if ( capacity < len )
+    {
+        ret = _msg_send(msg);
+        if ( ret != MSGBUF_PEER_ALIVE )
+        {
+            return ret;
+        }
+        return msg_send_line(msg, line, len);
+    }
+    else
+    {
+        memcpy(msg->buf + msg->end, line, len);
+        msg->end += len;
+        return MSGBUF_PEER_ALIVE;
+    }
+}
+MsgRet
+msg_recv_line(MsgBuf *msg,
+              char *line,    /* [o] */
+              int *len)      /* [o] */
+{
+    MsgRet ret = MSGBUF_PEER_ALIVE;
+    if ( (ret = _msg_recv(msg)) != MSGBUF_PEER_ALIVE )
+    {
+        return ret;
+    }
+
+    // End of Frame
+    if ( msg->buf[msg->start] == '\n' )
+    {
+        msg->start++;
+        strcpy(line, "\n");
+        *len = 1;
+        return MSGBUF_PEER_ALIVE;
+    }
+
+    // NOTE: If line contains multiple colons (:) , the last one is
+    //       treat as a delimiter.
+    // e.g.
+    //   When line is "foo:bar:baz", key is "foo:bar" and value is "baz".
+    char *pos = msg->buf + msg->start;
+    char *line_end = split_str(pos, '\n'); // "\n"'s pos + 1.
+    *len = line_end - pos - 1;
+    strcpy(line, pos);
+    msg->start = line_end - msg->buf;
+
+    return ret;
+}
+
+int
+establish_tcp_connection(char *ip, int port)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if ( sock == -1 )
+    {
+        perror("socket");
+        goto ERR;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    if ( inet_aton(ip, &addr.sin_addr) == 0 )
+    {
+        fprintf(stderr, "inet_aton: Invalid ipv4 address form.\n");
+        goto ERR;
+    }
+
+    if ( connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1 )
+    {
+        perror("connect");
+        goto ERR;
+    }
+END:
+    return sock;
+ERR:
+    if ( sock != -1 )
+    {
+        close(sock);
+    }
+    sock = -1;
+    goto END;
+}
+
+int
+socket_bind_listen(int port)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if ( sock == -1 )
+    {
+        perror("socket");
+        goto ERR;
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if ( bind(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1  )
+    {
+        perror("bind");
+        goto ERR;
+    }
+
+    if ( listen(sock, 10000) == -1 )
+    {
+        perror("listen");
+        goto ERR;
+    }
+
+END:
+    return sock;
+ERR:
+    if ( sock != -1 )
+    {
+        close(sock);
+    }
+    sock = -1;
+    goto END;
+}
+
+int
+accept_tcp_connection(int sock_listen)
+{
+    int sock_client = accept(sock_listen, NULL, NULL);
+    if ( sock_client == -1 )
+    {
+        perror("accept");
+        goto ERR;
+    }
+END:
+    return sock_client;
+ERR:
+    goto END;
+}
+
+#define LINE_256 "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234\n"
+
+#define SEND_NUM (1024*1024*1024/256)
+//#define SEND_NUM (1024*1024/256)
+//#define SEND_NUM (5)
+
+
+#ifdef SENDRECV
+int main(int argc, char *argv[])
+{
+    gLogger = logger_create();
+
+    (void)argc;
+    int sock = establish_tcp_connection("127.0.0.1", atoi(argv[1]));
+    if ( sock == -1 )
+    {
+        return 1;
+    }
+
+    int buflen = 1024 * 1024;
+
+    logger_start(gLogger);
+
+    char send_buf[buflen];
+    MsgBuf req;
+    msg_init(&req, send_buf, buflen, sock);
+    for ( int i = 0; i < SEND_NUM; i++ ) {
+        msg_send_line(&req, LINE_256, 256);
+    }
+    msg_send_line(&req, "\n", 1);
+    logger_end(gLogger);
+
+
+    logger_start(gLogger);
+
+    char recved[512] = { 0 };
+    int line_len;
+
+
+    char recv_buf[buflen];
+    MsgBuf res;
+    msg_init(&res, recv_buf, buflen, sock);
+    for ( int i = 0; i < SEND_NUM; i++ ) {
+        msg_recv_line(&req, recved, &line_len);
+        printf("%s\n", recved);
+    }
+    logger_end(gLogger);
+    return 0;
+}
+#endif
+#ifdef FDOPEN
+int main(int argc, char *argv[])
+{
+    gLogger = logger_create();
+
+    (void)argc;
+    int sock = establish_tcp_connection("127.0.0.1", atoi(argv[1]));
+    if ( sock == -1 )
+    {
+        return 1;
+    }
+
+    logger_start(gLogger);
+    FILE *fp = fdopen(sock, "r+");
+
+    for ( int i = 0; i < SEND_NUM; i++ ) {
+        fputs(LINE_256, fp);
+    }
+    fputs("\n", fp);
+    fflush(fp);
+
+    logger_end(gLogger);
+
+    logger_start(gLogger);
+
+    char recved[512];
+    for ( int i = 0; i < SEND_NUM; i++ ) {
+        fgets(recved, 512, fp);
+        printf("%s\n",recved);
+    }
+    logger_end(gLogger);
+    return 0;
+}
+#endif
+#ifdef PEER
+#define PEER_MSG "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcde\n"
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    int listen = socket_bind_listen(atoi(argv[1]));
+    int sock = accept_tcp_connection(listen);
+
+    FILE *fp = fdopen(sock, "r+");
+
+    char recved[512] = { 0 };
+    while ( fgets(recved, 512, fp) ) {
+        printf("%s\n",recved);
+        if ( strcmp(recved, "\n") == 0 )
+            break;
+    }
+    fflush(fp);
+
+    for ( int i = 0; i < SEND_NUM; i++ ) {
+        //send(sock, PEER_MSG, 256, 0);
+        fputs(PEER_MSG, fp);
+    }
+    fflush(fp);
+
+    while( 1 ) {
+        fgets(recved, 512, fp);
+        //printf("%s\n",recved);
+    }
+    fflush(fp);
+
+    return 0;
+}
+#endif
